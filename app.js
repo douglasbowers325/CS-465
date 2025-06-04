f4fceeda3b1a9477ef433685ee19eda4874f0ad2
Douglas Bowers <douglas.bowers@snhu.edu>
2025-06-03 21:04:29 -0400

    Module 5 completed baseline

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Define routers
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
 var handlebars = require('hbs');
 var app = express();
+var apiRouter = require('./app_api/routes/index');
+
+//Bring in the database
+require('./app_api/models/db');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
@@ -24,9 +29,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//wire-up routes to controllers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
